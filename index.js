2e12ae6992256b04cead7026d7554f8ce2ed0608
noaheakin <noaheakin@gmail.com>
2021-01-22 09:58:36 -0800

    commit tree b5d02bf065d6174181db9829f9107b330b719747

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions here
+function map(arr, callback) {
+    const results = []
+    for (let i = 0; i < arr.length; i ++) {
+        results.push(callback(arr[i]))
+    }
+    return results
+}
+
+// function reduce(arr, callback, start=0) {
+//     let counter = start
+//     for (let i = 0; i < arr.length; i ++) {
+//         counter = callback(arr[i], counter)
+//     }
+//     return counter
+// }
+
+function reduce(arr, callback, start) {
+    let results, i
+    (!!start) ? (results = start, i = 0) : (results = arr[0], i = 1)
+    for (; i < arr.length; i++) {
+        results = callback(arr[i], results)
+    }
+    return results;
+}
